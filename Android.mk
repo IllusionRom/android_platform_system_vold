lt03lte-cafaosp/platform-system-vold

Adding support of dm-req-crypt
Currently Android provides disk encryption support using dm-crypt
which is based on bios. dm-crypt uses 512 bytes packet size for
crypto operations. While 512 bytes size packet is ok for SW based
disk encryption, it is inefficient for HW based crypto engines.
dm-req-crypt is similar to dm-crypt except it uses block requests
rathe bios for crypto operations. block requests when unpacked
carries data upto 512KB. Hence, HW based crypto engine can be used
more efficiently.

Change-Id: Ic541d4d1b9db4ce32465e30c5f25619a80975767

Android.mk
@@ -71,6 +71,12 @@ LOCAL_C_INCLUDES := $(common_c_includes)
 
 LOCAL_CFLAGS := -Werror=format
 
+ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
+LOCAL_C_INCLUDES += device/qcom/common/cryptfs_hw
+common_shared_libraries += libcryptfs_hw
+LOCAL_CFLAGS += -DCONFIG_HW_DISK_ENCRYPTION
+endif
+
 LOCAL_SHARED_LIBRARIES := $(common_shared_libraries)
 
 LOCAL_STATIC_LIBRARIES := $(common_static_libraries)

IllusionRom/android_platform_system_vold

Android.mk
@@ -83,8 +83,16 @@ LOCAL_MODULE:= vold
 LOCAL_SRC_FILES := vold.c
 LOCAL_C_INCLUDES := $(common_c_includes)
 LOCAL_CFLAGS := $(common_cflags)
-LOCAL_SHARED_LIBRARIES := $(common_libraries) libcrypto
-LOCAL_STATIC_LIBRARIES := libvold $(common_static_libraries)
+
+ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
+LOCAL_C_INCLUDES += device/qcom/common/cryptfs_hw
+LOCAL_SHARED_LIBRARIES += libcryptfs_hw
+LOCAL_CFLAGS += -DCONFIG_HW_DISK_ENCRYPTION
+endif
+
+LOCAL_SHARED_LIBRARIES += $(common_libraries) libcrypto
+LOCAL_STATIC_LIBRARIES += libvold $(common_static_libraries)
+
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)